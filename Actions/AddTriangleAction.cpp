f256bd8d5d9cc858aaebcfee339a8eca812bfb1b
Ahmed_Hmdy <111378492+ahmada28@users.noreply.github.com>
2022-12-18 16:39:05 +0200

    Update AddTriangleAction.cpp

--- Actions/AddTriangleAction.cpp
+++ Actions/AddTriangleAction.cpp
@@ -1,4 +1,4 @@
-#include "AddTriangleAction.h"
+#include "AddTriangle.h"
 #include "..\Figures\CTriangle.h"
 
 #include "..\ApplicationManager.h"
@@ -6,46 +6,48 @@
 #include "..\GUI\input.h"
 #include "..\GUI\Output.h"
 
-AddTriangleAction::AddTriangleAction(ApplicationManager * pApp):Action(pApp)
+AddTriangle::AddTriangle(ApplicationManager* pApp) :Action(pApp)
 {}
 
-void AddTriangleAction::ReadActionParameters() 
-{	
+void AddTriangle::ReadActionParameters()
+{
 	//Get a Pointer to the Input / Output Interfaces
 	Output* pOut = pManager->GetOutput();
 	Input* pIn = pManager->GetInput();
 
-	pOut->PrintMessage("New Triangle: Click at first corner");	
-	//Read 1st corner and store in point P1
-	pIn->GetPointClicked(P1.x, P1.y);
+	pOut->PrintMessage("New Triangle: Click at first corner");
 
+	//Read 1st corner and store in point P1
+	pIn->GetPointClicked(Pc.x, Pc.y);
+	
 	pOut->PrintMessage("New Triangle: Click at second corner");
+
 	//Read 2nd corner and store in point P2
 	pIn->GetPointClicked(P2.x, P2.y);
 
-    pOut->PrintMessage("New Triangle: Click at second corner");
-	//Read 3nd corner and store in point P3
-	pIn->GetPointClicked(P3.x, P3.y);
+	pOut->PrintMessage("New Triangle: Click at third corner");
 
-	TrianglefGfxInfo.isFilled = false;	//default is not filled
+	//Read 3rd corner and store in point P3
+	pIn->GetPointClicked(P3.x, P3.y);
+	TriGfxInfo.isFilled = false;	//default is not filled
 	//get drawing, filling colors and pen width from the interface
-	TrianglefGfxInfo.DrawClr = pOut->getCrntDrawColor();
-	TrianglefGfxInfo.FillClr = pOut->getCrntFillColor();
+	TriGfxInfo.DrawClr = pOut->getCrntDrawColor();
+	TriGfxInfo.FillClr = pOut->getCrntFillColor();
 
 	pOut->ClearStatusBar();
 
 }
 
 //Execute the action
-void AddTriangleAction::Execute() 
+void AddTriangle::Execute()
 {
 	//This action needs to read some parameters first
 	ReadActionParameters();
-	
+
 	//Create a triangle with the parameters read from the user
-	CTriangle*T=new CTriangle(P1, P2,P3,TrianglefGfxInfo);
-	
+	CTriangle* R = new CTriangle(P1, P2,P3, TriGfxInfo);
 
 	//Add the triangle to the list of figures
-	pManager->AddFigure(T);
+	pManager->AddFigure(R);
 }
+
