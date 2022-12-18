cf21d55cd626ebb2dabba431279ac5ae7801b358
Ahmed_Hmdy <111378492+ahmada28@users.noreply.github.com>
2022-12-18 14:35:05 +0200

    Update CFigure.h

--- Figures/CFigure.h
+++ Figures/CFigure.h
@@ -18,8 +18,7 @@ public:
 	CFigure(GfxInfo FigureGfxInfo);
 
 	void SetSelected(bool s);	//select/unselect the figure
-	bool IsSelected() const;	//check whether fig is selected
-
+	virtual bool IsSelected() const=0;	//check whether fig is selected
 	virtual void Draw(Output* pOut) const  = 0 ;		//Draw the figure
 	
 	void ChngDrawClr(color Dclr);	//changes the figure's drawing color
