5a5aee7933b0911f952bd903ea5e77c23f7df370
salmamuhammede <117976110+salmamuhammede@users.noreply.github.com>
2022-12-30 10:57:06 +0200

    Update CCircle.h

--- Figures/CCircle.h
+++ Figures/CCircle.h
@@ -8,12 +8,21 @@ class CCircle : public CFigure
 private:
 	Point Center;
 	Point RadiusP;
+	Point undoc;
+	Point redoc;
+	Point undor;
+	Point redor;
+
 public:
 	CCircle(Point, Point, GfxInfo FigGfxInfo);
 	virtual void Draw(Output* pOut) const;
 	virtual void PrintInfo(Output* pOut) const;
 	virtual bool IsBelong( Point p) const;	//check whether point is on fig
-
+	virtual void Save(ofstream& OutFile) const;
+	virtual void Load(fstream& InFile);
+	virtual void MoveFigure(Point p); //transfer circle to the new position
+	virtual void cancelFigure();  //undo the movement
+	virtual void Redotransition(); //return the movement
 };
 
 #endif
