e5dea744b97262145a6ceec99657551422b6b335
salmamuhammede <117976110+salmamuhammede@users.noreply.github.com>
2022-12-30 11:15:40 +0200

    Update CSquare.cpp

--- Figures/CSquare.cpp
+++ Figures/CSquare.cpp
@@ -13,6 +13,9 @@ void CSquare::Draw(Output* pOut) const
 	pOut->DrawSqr(Center, FigGfxInfo, Selected);
 }
 
+
+
+
 void CSquare::PrintInfo(Output* pOut) const
 {
 	pOut->PrintMessage("SQUARE  ID   " + to_string(ID) + "Center(" + to_string(Center.x) + "  ,  " + to_string(Center.y) + ")   side length" + to_string(length));
@@ -22,4 +25,57 @@ bool CSquare::IsBelong( Point p)const
 {
 	return((p.x > Center.x - (length / 2.0)) && (p.x < Center.x + (length / 2.0)) && (p.y > Center.y - (length / 2.0)) && (p.y < Center.y + (length / 2.0)));
 }
+void CSquare::MoveFigure(Point p)
+{
+	undoc.x = Center.x;
+	undoc.y = Center.y;
+	redoc.x = p.x;
+	redoc.y = p.y;
+	Center.x = p.x;
+	Center.y = p.y;
+}
 
+void CSquare::Save(ofstream& OutFile) const
+{
+	string type = to_string(SQUARE);
+	string id = to_string(ID);
+	int draw_color = TranslateToInt(FigGfxInfo.DrawClr);
+	int fill = (FigGfxInfo.isFilled) ? TranslateToInt(FigGfxInfo.FillClr) : -1;
+
+	if (OutFile.is_open()) {
+		OutFile << setw(1) << type << " ";
+		OutFile << setw(3) << id << " ";
+		OutFile << setw(3) << Center.x << " ";
+		OutFile << setw(3) << Center.y << " ";
+		OutFile << setw(3) << draw_color << " ";
+		OutFile << setw(3) << fill << " ";
+		OutFile << '\n';
+	}
+}
+
+
+void CSquare::Load(fstream& InFile)
+{
+	int draw_color, fill;
+	if (InFile.is_open())
+	{
+		InFile >> ID;
+		InFile >> Center.x >> Center.y;
+		InFile >> draw_color >> fill;
+		if (fill == -1)
+			FigGfxInfo.isFilled = false;
+		else
+			ChngFillClr(TranslateToColor(fill));
+		ChngDrawClr(TranslateToColor(draw_color));
+	}
+}
+void CSquare::cancelFigure()
+{
+	Center.x = undoc.x;
+	Center.y = undoc.y;
+}
+void CSquare::Redotransition()
+{
+	Center.x = redoc.x;
+	Center.y = redoc.y;
+}
