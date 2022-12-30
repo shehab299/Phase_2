93894ffb516ae013456080498bf144d4d30a5d44
salmamuhammede <117976110+salmamuhammede@users.noreply.github.com>
2022-12-30 13:58:27 +0200

    Update CTriangle.h

--- Figures/CTriangle.h
+++ Figures/CTriangle.h
@@ -8,13 +8,32 @@ private:
 	Point Corner1;
 	Point Corner2;
 	Point Corner3;
+	Point Center;
+	Point undoCr1;
+	Point undoCr2;
+	Point undoCr3;
+	Point undoCen;
+	Point redoCr1;
+	Point redoCr2;
+	Point redoCr3;
+	Point redoCen;
+
+
+
+	float CalcArea(Point p1, Point p2, Point p3) const;
+
 
 public:
 	CTriangle(Point, Point, Point, GfxInfo FigureGfxInfo);
-	float CalcArea(Point p1, Point p2, Point p3)const;
 	virtual void Draw(Output* pOut) const;
 	virtual void PrintInfo(Output* pOut) const;
 	virtual bool IsBelong(Point p) const;
+	virtual void MoveFigure(Point p);
+	virtual void cancelFigure();
+	//void SetCenter();
+	virtual void Redotransition();
+	virtual void Save(ofstream& OutFile) const;
+	virtual void Load(fstream& InFile);
 };
 
 #endif
