5607b455fd496ec75f52dcc882a21c59c684d62a
salmamuhammede <117976110+salmamuhammede@users.noreply.github.com>
2022-12-30 11:02:03 +0200

    Update CFigure.h

--- Figures/CFigure.h
+++ Figures/CFigure.h
@@ -1,9 +1,15 @@
 #ifndef CFIGURE_H
 #define CFIGURE_H
 
+
+#include <fstream>
+#include <iomanip>
+
+
 #include "..\defs.h"
 #include "..\GUI\UI_Info.h"
 #include "..\GUI\Output.h"
+
 //Base class for all figures
 class CFigure
 {
@@ -16,23 +22,29 @@ protected:
 public:
 
 	CFigure(GfxInfo FigureGfxInfo);
+
 	void SetID(int id);
 	int GetID();
+
 	void SetSelected(bool s);	//select/unselect the figure
-	virtual bool IsBelong(Point p) const = 0;	//check whether point is on fig
 	bool IsSelected();
+
+	virtual bool IsBelong(Point p) const = 0;	//check whether point is on fig
 	virtual void Draw(Output* pOut) const  = 0 ;		//Draw the figure
+
 	void ChngDrawClr(color Dclr);	//changes the figure's drawing color
 	void ChngFillClr(color Fclr);	//changes the figure's filling color
-	//string GetDrawClr();  //gets the the figure's drawing color
-	//string GetFillClr();  //gets the the figure's filling color																		
-	//The following functions should be supported by the figure class
-	//It should be overridden by each inherited figure
-	//Decide the parameters that you should pass to each function	
-	//virtual void Save(ofstream &OutFile) = 0;	//Save the figure parameters to the file
-	//virtual void Load(ifstream &Infile) = 0;	//Load the figure parameters to the file
-
-	virtual void PrintInfo(Output* pOut)const = 0;	//print all figure info on the status bar
+	virtual void MoveFigure(Point p) = 0;      //Transfer each Figure to the new position
+	virtual void Save(ofstream &OutFile) const = 0 ;	//Save the figure parameters to the file
+	virtual void Load(fstream &Infile) = 0;	//Load the figure parameters to the file
+	virtual void PrintInfo(Output* pOut) const = 0;	//print all figure info on the status bar
+	virtual void cancelFigure()=0; //used to undo action move figure
+	virtual void Redotransition()=0; //used to redo action move figure
 };
+
+int TranslateToInt(color);
+color TranslateToColor(int);
+
+
 
 #endif
