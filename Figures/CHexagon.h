cb36c8c1b778492a6902ed21e3d8e2775145d378
salmamuhammede <117976110+salmamuhammede@users.noreply.github.com>
2022-12-30 11:09:00 +0200

    Update CHexagon.h

--- Figures/CHexagon.h
+++ Figures/CHexagon.h
@@ -3,17 +3,24 @@
 
 #include "CFigure.h"
 
+
 class CHexagon : public CFigure
 {
 private:
 	Point Center;
+	Point undop;
+	Point redop;
 	int length;
 public:
 	CHexagon(Point, GfxInfo FigureGfxInfo);
 	virtual void Draw(Output* pOut) const;
 	virtual void PrintInfo(Output* pOut) const;
 	virtual bool IsBelong( Point p) const;	//check whether point is on fig
-
+	virtual void Save(ofstream& OutFile) const;
+	virtual void Load(fstream& InFile);
+	virtual void MoveFigure(Point p);
+	virtual void cancelFigure();
+	virtual void Redotransition();
 };
 
 #endif
