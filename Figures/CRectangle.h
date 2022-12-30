9fa68406d686af08d9d2539987b51060d44fd5d9
salmamuhammede <117976110+salmamuhammede@users.noreply.github.com>
2022-12-30 11:10:47 +0200

    Update CRectangle.h

--- Figures/CRectangle.h
+++ Figures/CRectangle.h
@@ -1,6 +1,7 @@
 #ifndef CRECT_H
 #define CRECT_H
 
+
 #include "CFigure.h"
 
 class CRectangle : public CFigure
@@ -8,13 +9,22 @@ class CRectangle : public CFigure
 private:
 	Point Corner1;	
 	Point Corner2;
+	Point undoCorn1;
+	Point redoCorn1;
+	Point undoCorn2;
+	Point redoCorn2;
 public:
 
 	CRectangle(Point , Point, GfxInfo FigureGfxInfo );
 	virtual void Draw(Output* pOut) const;
 	virtual void PrintInfo(Output* pOut) const;
 	virtual bool IsBelong( Point p) const;	//check whether point is on fig
-
+	virtual void Save(ofstream& OutFile) const;
+	virtual void MoveFigure(Point p);
+	virtual void Load(fstream& InFile);
+	virtual void cancelFigure();
+	virtual void Redotransition();
+	
 };
 
 #endif
