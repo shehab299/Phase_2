8c6b8a06b650318eaa5215d41ff24a9e6ba14fac
Ahmed_Hmdy <111378492+ahmada28@users.noreply.github.com>
2023-01-01 00:27:25 +0200

    Update PickByColors.h

--- Actions/PickByColors.h
+++ Actions/PickByColors.h
@@ -2,18 +2,15 @@
 #include "Action.h"
 #include "../Figures/CFigure.h"
 
-class PickByColors:public Action
+class PickByColors :public Action
 {
 private:
 	CFigure* Fig;
-	int  rand_fig_no, NoPickedFig, flscntr, trucntr, clrs[6];
+	int  rand_fig_no, NoPickedFig, flscntr, trucntr;
 	Point P;
-	color AssignColor(CFigure* s);
-	void PrntScore(int);
-public :
+public:
 	PickByColors(ApplicationManager* pApp);
 	virtual void ReadActionParameters();
 	virtual void Execute();
 
 };
-
