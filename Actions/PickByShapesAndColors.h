5b0da8f7adf364d09edb17b5eb95669adb964cc0
Ahmed_Hmdy <111378492+ahmada28@users.noreply.github.com>
2023-01-01 00:28:17 +0200

    Update PickByShapesAndColors.h

--- Actions/PickByShapesAndColors.h
+++ Actions/PickByShapesAndColors.h
@@ -1,19 +1,17 @@
 #pragma once
 #include "Action.h"
-#include "..\Figures\CFigure.h"
+#include"../Figures/CFigure.h"
 class PickByShapesAndColors : public Action
 {
+
 private:
 	CFigure* Fig;
+	int  rand_fig_no, NoPickedFig, flscntr, trucntr;
 	Point P;
-	int  no_combs, rand_fig_no, picked_comb_no, trucntr, flscntr;
-	int combinations[23];
-	//void PrntScore(int);
 public:
 	PickByShapesAndColors(ApplicationManager* pApp);
 	virtual void ReadActionParameters();
 	virtual void Execute();
-	void PrntScore(int fd);
 
 };
 
