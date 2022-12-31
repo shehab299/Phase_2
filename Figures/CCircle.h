a2cb696868106e03a0b8e5323eada9f715661d67
Ahmed_Hmdy <111378492+ahmada28@users.noreply.github.com>
2022-12-31 12:08:18 +0200

    Final CCircle.h

--- Figures/CCircle.h
+++ Figures/CCircle.h
@@ -8,13 +8,15 @@ class CCircle : public CFigure
 private:
 	Point Center;
 	Point RadiusP;
+	int r;
 public:
 	CCircle(Point, Point, GfxInfo FigGfxInfo);
 	virtual void Draw(Output* pOut) const;
 	virtual void PrintInfo(Output* pOut) const;
 	virtual bool IsBelong( Point p) const;	//ch+eck whether point is on fig
 	virtual void Displacment(Point p) ; //dicplace the coordinates of the figure
-
+	virtual void Resize(Point p); //resize the figure
+	virtual string GetFigureType() const;
 };
 
 #endif
