21b073e50c8ed72218c9e571b6ae959d953d5b6d
Ahmed_Hmdy <111378492+ahmada28@users.noreply.github.com>
2022-12-31 12:04:19 +0200

    Final ApplicationManager.h

--- ApplicationManager.h
+++ ApplicationManager.h
@@ -14,7 +14,6 @@ class ApplicationManager
 private:
 	int FigCount;		//Actual number of figures
 	CFigure* FigList[MaxFigCount];	//List of all figures (Array of pointers)
-
 	CFigure* SelectedFig; //Pointer to the selected figure
 	//Pointers to Input and Output classes
 	Input *pIn;
@@ -35,12 +34,17 @@ public:
 	CFigure *GetFigure(int x1, int y1) const; //Search for a figure given a point inside the figure
 	void SetSelectedFig(CFigure* Fig);
 	CFigure* GetSelectedFig();
-	int GetFigCount();
-	CFigure* RetFig(int idx);
+	int GetFigCount(); // get the number of the figlist
+	int GetFilledFigCout(); // get the number of filled figs in the figlist 
+	int GetColorsCount(color c);  // get the filled figs in the figlist with a specific color
+	int GetSearchedFigCount(CFigure* Fig); // // get the number of a specific fig the figlist
+	int GetColoredFigCount(CFigure* Fig); //// get the number of a specific fig filled with a specific color in the figlist
+	CFigure* RetFig(int idx); // return a fig from the figlist with the passed index 
 	// -- Interface Management Functions
 	Input *GetInput() const; //Return pointer to the input
 	Output *GetOutput() const; //Return pointer to the output
-	void UpdateInterface() const;	//Redraws all the drawing window	
+	void UpdateInterface() const;	//Redraws all the drawing window	 
+	void ShowAllFigure();
 };
 
 #endif
