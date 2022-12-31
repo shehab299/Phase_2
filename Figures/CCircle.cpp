564e93bdd9ecff15e0100de8660f8f7f8f2e1a72
Ahmed_Hmdy <111378492+ahmada28@users.noreply.github.com>
2022-12-31 12:07:47 +0200

    Final CCircle.cpp

--- Figures/CCircle.cpp
+++ Figures/CCircle.cpp
@@ -4,6 +4,7 @@ CCircle::CCircle(Point cntr, Point p, GfxInfo FigGfxInfo) :CFigure(FigGfxInfo)
 {
 	Center = cntr;
 	RadiusP = p;
+ r = dist(Center, RadiusP);
 }
 void CCircle::Draw(Output* pOut) const
 {
@@ -12,23 +13,31 @@ void CCircle::Draw(Output* pOut) const
 
 void CCircle::PrintInfo(Output* pOut) const
 {
-	pOut->PrintMessage("CIRCLE ID   " + to_string(ID) + "    Center(" + to_string(Center.x) + "  ,  " + to_string(Center.y) + ")   raduis  " + to_string(int(sqrt(pow((Center.x - RadiusP.x), 2) + pow((Center.y - RadiusP.y), 2)))));
+	pOut->PrintMessage("CIRCLE ID   " + to_string(ID) + "    Center(" + to_string(Center.x) + "  ,  " + to_string(Center.y) + ")   raduis  " + to_string(r));
 }
 
 bool CCircle::IsBelong( Point p) const
 {
 	//check if the distance between the point and the center less than or equal the radius
-	int r = sqrt(pow((Center.x - RadiusP.x), 2) + pow((Center.y - RadiusP.y), 2));
-	int d = sqrt(pow((Center.x - p.x), 2) + pow((Center.y - p.y), 2));
+	int d = dist(Center,p);
 	return (d <= r);
 }
 
 void CCircle::Displacment(Point p) 
 {
-	int r = sqrt(pow((Center.x - RadiusP.x), 2) + pow((Center.y - RadiusP.y), 2));
 	RadiusP.x = p.x + r;
 	RadiusP.y = p.y;
 	Center.x = p.x;
 	Center.y = p.y;
+}
+
+void CCircle::Resize(Point p)
+{
+	RadiusP = p;
+}
+
+string CCircle::GetFigureType() const
+{
+	return "Circle";
 }
 
