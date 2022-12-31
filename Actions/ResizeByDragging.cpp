655cb8ff513bdd7a7936eba9112a19fe8279ce8f
Ahmed_Hmdy <111378492+ahmada28@users.noreply.github.com>
2023-01-01 00:23:12 +0200

    Update ResizeByDragging.cpp

--- Actions/ResizeByDragging.cpp
+++ Actions/ResizeByDragging.cpp
@@ -1,18 +1,35 @@
 #include "ResizeByDragging.h"
-
 #include "..\ApplicationManager.h"
-
 #include "..\GUI\input.h"
 #include "..\GUI\Output.h"
 
 ResizeByDragging::ResizeByDragging(ApplicationManager* pApp) :Action(pApp)
-{
-}
+{}
 
 void ResizeByDragging::ReadActionParameters()
 {
+	Output* pOut = pManager->GetOutput();
+	Input* pIn = pManager->GetInput();
+	pOut->PrintMessage("Resize By Dragging Action:Please drag the selected figure from a corner");
+		while (pIn->GetPointDragged(p.x, p.y) == BUTTON_UP)
+		{
+			while (pIn->GetPointDragged(p.x, p.y) == BUTTON_DOWN)
+			{
+				pManager->GetSelectedFig()->Resize(p);
+				pManager->UpdateInterface();
+			}
+		}
+	pOut->ClearStatusBar();
 }
 
 void ResizeByDragging::Execute()
 {
+	Output* pOut = pManager->GetOutput();
+	if (pManager->GetSelectedFig()->IsSelected())
+	{
+		ReadActionParameters();
+		pManager->GetSelectedFig()->Displacment(p);
+	}
+	else
+		pOut->PrintMessage("Please select a figure first");
 }
