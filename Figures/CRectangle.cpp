98bdbdd03ddbf6b52e4221cf6b03eb13111d4740
Ahmed_Hmdy <111378492+ahmada28@users.noreply.github.com>
2022-12-31 12:15:05 +0200

    Final CRectangle.cpp

--- Figures/CRectangle.cpp
+++ Figures/CRectangle.cpp
@@ -14,7 +14,8 @@ void CRectangle::Draw(Output* pOut) const
 
 void CRectangle::PrintInfo(Output* pOut) const
 {
-	pOut->PrintMessage("RECTANGLE ID   " + to_string(ID) + "   " + to_string(Corner1.x) + "   " + to_string(Corner1.y) + "   " + to_string(Corner2.x) + "   " + to_string(Corner2.y));
+	int width = abs(Corner1.x - Corner2.x), length = abs(Corner1.x - Corner2.x);
+	pOut->PrintMessage("RECTANGLE ID   " + to_string(ID) + "   " + to_string(Corner1.x) + "   " + to_string(Corner1.y) + "   " + to_string(Corner2.x) + "   " + to_string(Corner2.y)+"   Width  "+to_string(width)+"   Length  "+to_string(length));
 }
 
 bool CRectangle::IsBelong(Point p) const
@@ -34,4 +35,19 @@ void CRectangle::Displacment(Point p)
 	Corner2.y = p.y + DeltaY;
 	Corner1.x = p.x - DeltaX;
 	Corner1.y = p.y - DeltaY;
+}
+
+void CRectangle::Resize(Point p)
+{
+	int d1 = dist(Corner1, p);
+	int d2 = dist(Corner2, p);
+	if (d1 <= d2)
+		Corner1 = p;
+	else
+		Corner2 = p;
+}
+
+string CRectangle::GetFigureType() const
+{
+	return "Rectangle";
 }
